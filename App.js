aa65a172d435b439e9261eaa29898798cea515f6
Hendra Kurniawan <i@hendrakurniawan.com>
2018-09-25 19:18:28 +0700

    Add AppLoading component

--- App.js
+++ App.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
 
 import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  }
+
+  _loadResourcesAsync = () => {
+    return Promise.all([
+      Font.loadAsync({
+        ...Icon.Ionicons.font
+      })
+    ]);
+  }
+
+  _handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        { Platform.OS === 'ios' && <StatusBar barStyle='default'/> }
-        <AppNavigator />
-      </View>
-    );
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          { Platform.OS === 'ios' && <StatusBar barStyle='default'/> }
+          <AppNavigator />
+        </View>
+      );
+    }
   }
 }
 
